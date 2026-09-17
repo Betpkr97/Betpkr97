2474750c99e22812a691d7da0b25505781d9675b
Betpkr97 <murshadgamingyt12@gmail.com>
2026-09-17 09:31:14 +0500

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,23 @@
 import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
+app.use(express.json());
+
+// public folder ki website files
+app.use(express.static(path.join(__dirname, "public")));
+
+// Homepage
 app.get("/", (req, res) => {
-  res.send("BETPKR website is working!");
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
+
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
