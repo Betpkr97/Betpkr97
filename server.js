1a37a56bcc694af131fdfd67429e5eadfbf2c889
Betpkr97 <murshadgamingyt12@gmail.com>
2026-09-17 06:03:18 +0500

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,12 @@
+import express from "express";
+
+const app = express();
+
 const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("BETPKR website is working!");
+});
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
